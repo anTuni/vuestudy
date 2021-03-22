ecc1dd02f91dd368a8a370b357a874a0db11be80
Tune <cfdw011@gmail.com>
2021-03-22 14:48:22 +0900

    attribute binding

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ var app=new Vue({
     el :'#app',
     data:{
         product : 'Socks',
-        description : 'are smells good'
+        description : 'are smells good',
+        cb:"color-blue",
+        href: "index2.html",
+        class: "index",
     }
 })
 
-var productgd=new Vue({
+var app2=new Vue({
     el :'#app2',
     data:{
         product : 'app2',
