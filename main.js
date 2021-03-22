bd5d612609b30fc7b08b330f53ef0f6f06f9fda6
Tune <cfdw011@gmail.com>
2021-03-22 14:25:24 +0900

    veu instant-commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var app=new Vue({
+    el :'#app',
+    data:{
+        product : 'Socks',
+        description : 'are smells good'
+    }
+})
+
+var productgd=new Vue({
+    el :'#app2',
+    data:{
+        product : 'app2',
+        value1 : 1,
+        value2 : 2,
+        description : 'app2 is warm'
+    }
+})
